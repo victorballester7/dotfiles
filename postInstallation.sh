ab53b0a8e72092619f6b5544ada9c705f352bd67
Víctor <victor.ballester.ribo@gmail.com>
2023-09-22 20:13:02 +0200

    updated installation and backup files with colors

--- postInstallation.sh
+++ postInstallation.sh
@@ -1,74 +1,177 @@
 #!/bin/bash
 
 # Remember to previously install firefox and git and also reboot at the end of the execution of this file.
+# You can execute this file from anywhere.
 # This is for Archlinux + KDE
 
+# ANSI escape codes for color formatting
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+RESET="\e[0m"
 
 # update & upgrade
-sudo pacman -Syyu
+echo -e "${YELLOW}Updating and upgrading...${RESET}"
+sudo pacman -Syyu --noconfirm --needed --quiet
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error updating and upgrading.${RESET}"
+    notify-send "Error updating and upgrading" "There was a problem updating and upgrading. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Updated and upgraded successfully.${RESET}"
+fi
 
 # scripts
+cd
+echo -e "${YELLOW}Moving scripts...${RESET}"
+cd Desktop/dotfiles/
 chmod +x move_scripts.sh
 ./move_scripts.sh
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error moving scripts.${RESET}"
+    notify-send "Error moving scripts" "There was a problem moving scripts. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Scripts moved successfully.${RESET}"
+fi
+
+
 
 
 # Git + Github keys
-sudo pacman -S --needed git
-cp others/.gitconfig ~/
-echo "Press enter when promted to 'Enter a file in which to save the key', and in 'add passphrase'"
+cd
+echo -e "${YELLOW}Installing and configuring git...${RESET}"
+sudo pacman -S --noconfirm --needed --quiet git
+cp Desktop/dotfiles/others/.gitconfig ~/
+echo -e "${BLUE}Press enter when promted to 'Enter a file in which to save the key', and in 'add passphrase'${RESET}"
 ssh-keygen -t ed25519 -C "victor.ballester.ribo@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-echo "Now copy the following key to your Github account in Settings/Access/Ssh keys..."
+echo -e "${BLUE}Now copy the following key to your Github account in Settings/Access/Ssh keys...${RESET}"
 cat ~/.ssh/id_ed25519.pub
-printf "%s " "Press enter to continue once copied the key to Github"
+echo -e "${BLUE}Press enter to continue once copied the key to Github${RESET}"
 read ans
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error installing and configuring git.${RESET}"
+    notify-send "Error installing and configuring git" "There was a problem installing and configuring git. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Git installed and configured successfully.${RESET}"
+fi
 
 # paru (AUR helper)
-sudo pacman -S --noconfirm --needed base-devel
+cd
+echo -e "${YELLOW}Installing paru...${RESET}"
+sudo pacman -S --noconfirm --needed --quiet base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
 rm -rf paru
-
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error installing paru.${RESET}"
+    notify-send "Error installing paru" "There was a problem installing paru. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Paru installed successfully.${RESET}"
+fi
 
 # PROBLEM - SOLVING
 # Audio drivers:
 cd
-paru -S --noconfirm --needed sof-firmware alsa-ucm-conf
+echo -e "${YELLOW}Installing audio drivers...${RESET}"
+paru -S --noconfirm --needed --quiet sof-firmware alsa-ucm-conf
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error installing audio drivers.${RESET}"
+    notify-send "Error installing audio drivers" "There was a problem installing audio drivers. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Audio drivers installed successfully.${RESET}"
+fi
 
 # If it take too long to reboot and/or shutdown then is may be a problem of sddm
 # To solve this, install a newer version of sddm from AUR: sddm-git
-paru -S sddm-git
+# paru -S sddm-git
 
 
 # Copy rules (that will be executed at the startup of the system):
 cd
-sudo cp Desktop/dotfiles/Rules/* /etc/udev/rules.d/
+echo -e "${YELLOW}Copying rules...${RESET}"
+sudo cp Desktop/dotfiles/rules/* /etc/udev/rules.d/
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error copying rules.${RESET}"
+    notify-send "Error copying rules" "There was a problem copying rules. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Rules copied successfully.${RESET}"
+fi
 
 # Restore KDE backup
-cd Desktop
-git clone https://gitlab.com/cscs/transfuse.git
-cd transfuse
-chmod +x transfuse.sh
-cp ../dotfiles/config/*.tar.gz ./
-./transfuse.sh
+# old version ------------------
+# cd Desktop
+# git clone https://gitlab.com/cscs/transfuse.git
+# cd transfuse
+# chmod +x transfuse.sh
+# cp ../dotfiles/config/*.tar.gz ./
+# ./transfuse.sh
+# 
+# new version ------------------
+cd 
+cd Desktop/dotfiles/config/
+echo -e "${YELLOW}Restoring KDE backup...${RESET}"
+paru -S --noconfirm --needed konsave
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error installing konsave.${RESET}"
+    notify-send "Error installing konsave" "There was a problem installing konsave. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Konsave installed successfully.${RESET}"
+fi
+konsave -i victor-config.knsv
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error restoring KDE backup.${RESET}"
+    notify-send "Error restoring KDE backup" "There was a problem restoring KDE backup. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}KDE backup restored successfully.${RESET}"
+fi
 
+
+
+# Necessary packages (already included in the list below):
+# for weather widget: qt5-xmlpatterns qt5-xmlpatterns
+# for kde-connect transfer protocol: sshfs
+# for rclone: fuse2
+# for vscode: perl cpanminus cmake make
+
+# Other interesting but not included packages:
+# howdy (face recognition)
 
 # Installation of programs
 cd 
-# Others: autorandr
-paru -S --noconfirm --needed autopep8 cheese chromium clang discord ethtool gcc gdb gimp gnome-screenshot gnome-terminal howdy inkscape jdk-openjdk jupyter-notebook kdeconnect kde-graphics-meta latte-dock libreoffice-fresh man-db man-pages neofetch ntfs-3g octave partitionmanager python qbittorrent rclone rsync teams-for-linux texlive tlp visual-studio-code-bin vlc zsh
 
+# List of programs to install
+programs="autopep8 cheese chromium clang cmake cpanminus discord ethtool firefox fuse2 gcc gdb gimp gnome-screenshot gnome-terminal inkscape jdk-openjdk jupyter-notebook kde-graphics-meta kdeconnect kdenlive latte-dock libreoffice-fresh make man-db man-pages neofetch ntfs-3g octave partitionmanager perl python qbittorrent qt5-xmlpatterns qt5-xmlpatterns rclone rsync sshfs teams-for-linux texlive tlp visual-studio-code-bin vlc zsh"
 
-# Modules necessary:
-# for weather widget
-paru -S --noconfirm --needed qt5-xmlpatterns qt5-xmlpatterns 
+# Loop through each program and install it
+for program in $programs; do
+    echo -e "${YELLOW}Installing $program...${RESET}"
+    paru -S --noconfirm --needed "$program"
 
-# for kde-connect transfer protocol
-paru -S --noconfirm --needed sshfs
+    # Check the exit status of the previous command
+    if [ $? -ne 0 ]; then
+        # If an error occurred, display an alert in red
+        echo -e "${RED}Error installing $program${RESET}"
+        notify-send "Error installing $program" "There was a problem installing $program. Please check the installation process." --urgency=critical
+    else
+        # If successful, display a success message in green
+        echo -e "${GREEN}$program installed successfully.${RESET}"
+    fi
+done
 
+# Copy my custom PATH variables
+cd
+echo -e "${YELLOW}Copying profile...${RESET}"
+sudo cp Desktop/dotfiles/others/profile /etc/
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error copying profile.${RESET}"
+    notify-send "Error copying profile" "There was a problem copying profile. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Profile copied successfully.${RESET}"
+fi
 
 # # auto-cpufreq (battery powersave) (use either this or tlp; NOT both)
 # systemctl start auto-cpufreq
@@ -102,13 +205,15 @@ paru -S --noconfirm --needed sshfs
 
 # with the line
 
+# in order to mask the interupt /sys/firmware/interupt/gpe6E because it was causing many interupts when the laptop was plugged in. Now it goes like a charm! :)
 # GRUB_CMDLINE_LINUX_DEFAULT="acpi_mask_gpe=0x6E loglevel=3 quiet"
 
-# in order to mask the interupt /sys/firmware/interupt/gpe6E because it was causing many interupts when the laptop was plugged in. Now it goes like a charm! :)
 sudo cp Desktop/dotfiles/others/grub /etc/default/
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # inskcape (vector graphics editor)
+echo -e "${YELLOW}Configuring inkscape...${RESET}"
+mkdir -p ~/.config/inkscape/
 
 # kdeconnect
 
@@ -121,6 +226,10 @@ sudo grub-mkconfig -o /boot/grub/grub.cfg
 # latte-dock (my dock for linux)
 
 # man-db, man-pages: implement man pages on Arch
+
+# mpv (it's missing to copy and update the scripts in .config/mpv)
+# paru -S --needed trash-cli # in order to use delete property
+# paru -S --needed mediainfo # in order to use all the properties of comand-palette (source: https://github.com/stax76/mpv-scripts/tree/main)
 
 # neofetch
 sudo cp Desktop/dotfiles/others/neofetch /usr/bin/
@@ -139,13 +248,21 @@ sudo cp Desktop/dotfiles/others/pacman.conf /etc/
 # Notes: Name the remote as "OneDrive".
 #        Do not change the folder to /etc/systemd/system/ because otherwise it will compile as root and we don't want this (because we would have to change other things too).
 #        If an error about fusermount is prompt, install fuse2.
-paru -S --noconfirm --needed fuse2
+cd
+echo -e "${YELLOW}Installing rclone...${RESET}"
+echo -e "${BLUE}Name the remote as 'OneDrive'${RESET}"
 rclone config
 mkdir -p ~/.config/systemd/user/
 mkdir -p ~/OneDrive
 sudo cp Desktop/dotfiles/others/rclone-onedrive.service ~/.config/systemd/user/
 systemctl --user daemon-reload
 systemctl --user enable --now rclone-onedrive
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error installing rclone.${RESET}"
+    notify-send "Error installing rclone" "There was a problem installing rclone. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Rclone installed successfully.${RESET}"
+fi
 
 # rsync (necessary for the backup script: https://gitlab.com/cscs/transfuse)
 
@@ -163,29 +280,64 @@ systemctl --user enable --now rclone-onedrive
 # sudo usermod -a -G users $USER
 
 # tlp (battery management)
+echo -e "${YELLOW}Configuring tlp...${RESET}"
 systemctl mask systemd-rfkill.service
 systemctl mask systemd-rfkill.socket
 systemctl enable tlp.service
 sudo tlp start
 sudo cp Desktop/dotfiles/others/tlp.conf /etc/
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error configuring tlp.${RESET}"
+    notify-send "Error configuring tlp" "There was a problem configuring tlp. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Tlp configured successfully.${RESET}"
+fi
 
 # vscode (latexindent formatter configuration)
-cd ~/Desktop 
+cd 
+echo -e "${YELLOW}Configuring vscode...${RESET}"
 git clone git@github.com:cmhughes/latexindent.pl.git
 cd latexindent.pl
-sudo paru -S --noconfirm --needed perl cpanminus cmake make
 sudo perl helper-scripts/latexindent-module-installer.pl
 cmake path-helper-files
 sudo make install
 cd 
-rm -rf Desktop/latexindent.pl
+rm -rf latexindent.pl
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error configuring vscode.${RESET}"
+    notify-send "Error configuring vscode" "There was a problem configuring vscode. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Vscode configured successfully.${RESET}"
+fi
 
 # vlc
+cd
+echo -e "${YELLOW}Configuring vlc...${RESET}"
 mkdir ~/.config/vlc/
 sudo cp Desktop/dotfiles/others/vlcrc ~/.config/vlc/
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error configuring vlc.${RESET}"
+    notify-send "Error configuring vlc" "There was a problem configuring vlc. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Vlc configured successfully.${RESET}"
+fi
 
 # zsh 
+echo -e "${YELLOW}Configuring zsh...${RESET}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo cp Desktop/dotfiles/others/.zshrc ~/
 chsh -s /bin/zsh
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Error configuring zsh.${RESET}"
+    notify-send "Error configuring zsh" "There was a problem configuring zsh. Please check the installation process." --urgency=critical
+else
+    echo -e "${GREEN}Zsh configured successfully.${RESET}"
+fi
+echo -e "${BLUE}Remember to install Maple after rebooting.${RESET}"
+# wait for 5 seconds
+sleep 5
+# reboot
+echo -e "${YELLOW}Rebooting...${RESET}"
+sleep 1
+reboot
--- preInstallation.sh
+++ preInstallation.sh
@@ -1,35 +1,89 @@
 #!/bin/bash
 
+# ANSI escape codes for color formatting
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+RESET="\e[0m"
+
 # .config folder (copy KDE configs files)
-rm ~/Desktop/dotfiles/config/*.tar.gz
-cd ~/Desktop
-git clone https://gitlab.com/cscs/transfuse.git
-cd transfuse
-chmod +x transfuse.sh
-./transfuse.sh -b victor
-./transfuse.sh -p
-cp *.tar.gz ../dotfiles/config
-cp *.txt ../dotfiles/config
+# rm ~/Desktop/dotfiles/config/*.tar.gz
+# cd ~/Desktop
+# git clone https://gitlab.com/cscs/transfuse.git
+# cd transfuse
+# chmod +x transfuse.sh
+# ./transfuse.sh -b victor
+# ./transfuse.sh -p
+# cp *.tar.gz ../dotfiles/config
+# cp *.txt ../dotfiles/config
+
+echo -e "${YELLOW}Copying KDE config files...${RESET}"
+paru -S --noconfirm --needed --quiet konsave
+konsave -s victor-config
+konsave -e victor-config -d ~/Desktop/dotfiles/config
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}KDE config files copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying KDE config files!${RESET}"
+fi
+
 
 # Copy history
 cd ~/Desktop/dotfiles
+echo -e "${YELLOW}Copying history...${RESET}"
 history > history/history.txt
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}History copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying History!${RESET}"
+fi
 
 # Git
-cp ~/.gitconfig  others/
+cp ~/.gitconfig others/
+echo -e "${YELLOW}Copying Git config file...${RESET}"
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Git config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying Git config file!${RESET}"
+fi
 
 # grub
+echo -e "${YELLOW}Copying Grub config file...${RESET}"
 sudo cp -f /etc/default/grub others/
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Grub config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying Grub config file!${RESET}"
+fi
 
 # neofetch
+echo -e "${YELLOW}Copying Neofetch config file...${RESET}"
 sudo cp /usr/bin/neofetch others/ 
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Neofetch config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying Neofetch config file!${RESET}"
+fi
 
 # OneDrive sync
+echo -e "${YELLOW}Copying OneDrive sync config file...${RESET}"
 cp ~/.config/systemd/user/rclone-onedrive.service others/
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}OneDrive sync config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying OneDrive sync config file!${RESET}"
+fi
 
 
 # Pacman conf
+echo -e "${YELLOW}Copying Pacman config file...${RESET}"
 sudo cp /etc/pacman.conf others/
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Pacman config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying Pacman config file!${RESET}"
+fi
 
 # qbittorrent 
 
@@ -37,13 +91,38 @@ sudo cp /etc/pacman.conf others/
 # cp ~/.sage/init.sage others/
 
 # tlp (battery management)
+echo -e "${YELLOW}Copying TLP config file...${RESET}"
 cp /etc/tlp.conf others/
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}TLP config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying TLP config file!${RESET}"
+fi
 
 # vlc
+echo -e "${YELLOW}Copying VLC config file...${RESET}"
 cp ~/.config/vlc/vlcrc others/
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}VLC config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying VLC config file!${RESET}"
+fi
 
 # zsh 
+echo -e "${YELLOW}Copying Zsh config file...${RESET}"
 cp ~/.zshrc others/
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Zsh config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying Zsh config file!${RESET}"
+fi
 
-## NOTES
-# 1. Consider backing up the file /etc/profile (where there are my curstom PATH variables)
+
+# custom PATH variables
+echo -e "${YELLOW}Copying Custom PATH variables config file...${RESET}"
+cp /etc/profile others/
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Custom PATH variables config file copied successfully!${RESET}"
+else
+    echo -e "${RED}Error copying Custom PATH variables config file!${RESET}"
+fi
