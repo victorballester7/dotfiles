5ea193cd587f2e2cd96e2efc5597f31772ec99e6
Víctor <victor.ballester.ribo@gmail.com>
2024-04-29 20:09:50 +0200

    updated updateJPG->jpg to recursive

--- scripts/changeJPG2jpg.sh
+++ scripts/changeJPG2jpg.sh
@@ -1,8 +1,29 @@
 # script to change the names ***.JPG to ***.jpg or ***.MP4 to ***.mp4 of all files in a directory but do it also for all subdirectories
 
+change_files() {
+  # change all JPG to jpg
+  for i in $(find . -name "*.JPG"); do mv $i ${i%.*}.jpg; done
 
-# change all JPG to jpg
-for i in $(find . -name "*.JPG"); do mv $i ${i%.*}.jpg; done
+  # change all MP4 to mp4
+  for i in $(find . -name "*.MP4"); do mv $i ${i%.*}.mp4; done
+}
 
-# change all MP4 to mp4
-for i in $(find . -name "*.MP4"); do mv $i ${i%.*}.mp4; done
+# pass the folder as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <folder>"
+    exit 1
+fi
+
+cd $1
+base_folder=$(pwd)
+
+depth=3
+folders=$(find ./ -maxdepth $depth -type d) 
+
+for folder in $folders; do
+    echo -e "${YELLOW}Processing $folder${RESET}"
+    cd $folder
+    change_files
+    echo -e "${GREEN}Done${RESET}"
+    cd $base_folder
+done
