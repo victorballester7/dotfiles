fa6888cff99e9efd040ba53215de11e0fb64afba
Victor Ballester <victor.ballester.ribo@gmail.com>
2026-03-07 13:53:48 +0000

    a few things

--- config/hypr.quickshell.bak/scripts/monitorAttached.sh
+++
@@ -1,3 +0,0 @@
-# we add first moveWorkspacesToMonitors.sh because get_bing_image has "exit" commands in it
-# source $HOME/.config/hypr/scripts/moveWorkspacesToMonitors.sh
-source $HOME/.config/hypr/scripts/get_bing_image.sh
